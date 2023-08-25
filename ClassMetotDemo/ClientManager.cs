0c129b4eca412c380e81c45f99d94b243ec6e0a9
senakaratss <karatasssena@gmail.com>
2023-08-25 16:57:03 +0300

    Add project files.

---
+++ ClassMetotDemo/ClientManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine(customer.Name+" "+ customer.Surname+"("+ customer.Id+")" +"has been added.");
+        }
+        public void DeleteCustomer(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + "(" + customer.Id + ")" + "has been deleted.");
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ClassMetotDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Sena";
+            customer1.Surname = "Karataş";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Serena";
+            customer2.Surname = "Williams";
+
+            Customer[] customers = new Customer[] { customer1, customer2 };
+            foreach(var customer in customers)
+            {
+                Console.WriteLine(customer.Name);
+            }
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.AddCustomer(customer1);
+            customerManager.DeleteCustomer(customer2);
+        }
+    }
+}
+
+
+
+
+
+    
